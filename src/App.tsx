e2867e556679ba8b7f853f90426074f8a321b1dd
Augustine <ashramwen@gmail.com>
2022-08-03 10:30:22 +0800

    feat: Update theme

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,35 @@ function App() {
   console.log('🚀 ~ App ~ redman', redman);
 
   return (
-    <div className="mockup-window mx-96 my-8 border bg-base-300">
-      <div className="flex justify-center bg-base-200 px-4 py-16">
-        <div className="form-control gap-2">
-          <label className="input-group">
-            <span>生日</span>
-            <input type="text" placeholder="Birthday" className="input input-bordered" />
-          </label>
+    <div className="mx-auto my-12 w-1/4">
+      <div className="mockup-window border bg-base-300">
+        <div className="flex justify-center bg-base-200 px-4 py-16">
+          <div className="form-control space-y-2">
+            <label className="input-group">
+              <span>生日</span>
+              <input type="text" placeholder="Birthday" className="input input-bordered" />
+            </label>
 
-          <label className="input-group">
-            <span>性別</span>
-            <input type="text" placeholder="Gender" className="input input-bordered" />
-          </label>
+            <label className="input-group">
+              <span>性別</span>
+              <input type="text" placeholder="Gender" className="input input-bordered" />
+            </label>
 
-          <label className="input-group">
-            <span>身高</span>
-            <input type="text" placeholder="Height" className="input input-bordered" />
-          </label>
+            <label className="input-group">
+              <span>身高</span>
+              <input type="text" placeholder="Height" className="input input-bordered" />
+            </label>
 
-          <label className="input-group">
-            <span>體重</span>
-            <input type="text" placeholder="Weight" className="input input-bordered" />
-          </label>
+            <label className="input-group">
+              <span>體重</span>
+              <input type="text" placeholder="Weight" className="input input-bordered" />
+            </label>
 
-          <label className="input-group">
-            <span>體脂</span>
-            <input type="text" placeholder="Body fat" className="input input-bordered" />
-          </label>
+            <label className="input-group">
+              <span>體脂</span>
+              <input type="text" placeholder="Body fat" className="input input-bordered" />
+            </label>
+          </div>
         </div>
       </div>
     </div>
