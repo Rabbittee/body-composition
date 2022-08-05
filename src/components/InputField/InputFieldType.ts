ed84815333ca14f8b83171fbb25ab2e58d7b7132
Augustine <ashramwen@gmail.com>
2022-08-06 02:09:00 +0800

    fix: Fix Birth type incorrect issues

--- src/components/InputField/InputFieldType.ts
+++ src/components/InputField/InputFieldType.ts
@@ -21,7 +21,7 @@ type FieldTypeMap = {
 };
 
 export const INPUT_TYPE: FieldTypeMap = {
-  [InputFieldType.Birth]: { text: '生日', placeholder: '2001-01-01' },
+  [InputFieldType.Birth]: { type: 'date', text: '生日', placeholder: '2001-01-01' },
   [InputFieldType.Height]: { text: '身高', placeholder: '175' },
   [InputFieldType.Weight]: { text: '體重', placeholder: '70' },
   [InputFieldType.BodyFat]: { text: '體脂', placeholder: '20' },
