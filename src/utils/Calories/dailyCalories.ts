c8eb0e97d354ed67a61a20e9242c751a16260b31
Augustine <ashramwen@gmail.com>
2022-08-13 23:51:22 +0800

    refactor: Refactor dailyCalories

--- src/utils/Calories/dailyCalories.ts
+++ src/utils/Calories/dailyCalories.ts
@@ -1,27 +1,38 @@
 import Decimal from 'decimal.js';
 import { BodyInfo, Activity, Gender } from 'models';
 
-import * as BMRFunc from '../BMR';
-import { CaloriesParameters } from './parameters';
+import { BMR } from '../';
+
+export const CaloriesParameters = {
+  [Gender.Male]: {
+    [Activity.Sedentary]: 1.3,
+    [Activity.Light]: 1.6,
+    [Activity.Moderate]: 1.7,
+    [Activity.Active]: 2.1,
+    [Activity.VeryActive]: 2.4,
+  },
+  [Gender.Female]: {
+    [Activity.Sedentary]: 1.3,
+    [Activity.Light]: 1.5,
+    [Activity.Moderate]: 1.6,
+    [Activity.Active]: 1.9,
+    [Activity.VeryActive]: 2.2,
+  },
+};
 
 //round(min(0.007184*(height*0.725)*(weight*0.425)*24*(vlookup(BMI(age, gender))*(actionvalue(actionType, gender)))
 export function calDailyCalories(bodyInfo: BodyInfo) {
   const { gender, activity } = bodyInfo;
+  const ratio = CaloriesParameters[gender][activity];
 
   try {
-    const BMR = Object.values(BMRFunc).map((func) => func(bodyInfo));
+    const _BMR = Object.values(BMR).map((func) => func(bodyInfo));
 
     const min = Math.min(
-      ...BMR.map((val) => (typeof val !== 'string' ? Number(val[1]) : Number(val)))
+      ..._BMR.map((val) => (typeof val === 'string' ? Number(val) : Number(val[1])))
     );
 
-    const index = Object.values(Activity).indexOf(activity);
-
-    if (gender === Gender.Male) {
-      return new Decimal(min).times(CaloriesParameters.Male[index]).round().toString();
-    }
-
-    return new Decimal(min).times(CaloriesParameters.Female[index]).round().toString();
+    return new Decimal(min).times(ratio).round().toString();
   } catch {
     return '-';
   }
