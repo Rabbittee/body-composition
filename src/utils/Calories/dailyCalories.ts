29b55a932fd6537711fee6ab6ddcf8b5c6ea746f
Alpha <46735230+alphatero@users.noreply.github.com>
2022-08-13 23:56:46 +0800

    feat: 🎸 add calories caculator (#34)
    * feat: 🎸 add calories caculator
    
    * refactor: Refactor dailyCalories
    
    * refactor: Remove unused file
    
    Co-authored-by: Augustine <ashramwen@gmail.com>

--- src/utils/Calories/dailyCalories.ts
+++ src/utils/Calories/dailyCalories.ts
@@ -1,5 +1,39 @@
-import { BodyInfo } from 'models';
+import Decimal from 'decimal.js';
+import { BodyInfo, Activity, Gender } from 'models';
 
+import { BMR } from '../';
+
+export const CaloriesParameters = {
+  [Gender.Male]: {
+    [Activity.Sedentary]: 1.3,
+    [Activity.Light]: 1.6,
+    [Activity.Moderate]: 1.7,
+    [Activity.Active]: 2.1,
+    [Activity.VeryActive]: 2.4,
+  },
+  [Gender.Female]: {
+    [Activity.Sedentary]: 1.3,
+    [Activity.Light]: 1.5,
+    [Activity.Moderate]: 1.6,
+    [Activity.Active]: 1.9,
+    [Activity.VeryActive]: 2.2,
+  },
+};
+
+//round(min(0.007184*(height*0.725)*(weight*0.425)*24*(vlookup(BMI(age, gender))*(actionvalue(actionType, gender)))
 export function calDailyCalories(bodyInfo: BodyInfo) {
-  return '-';
+  const { gender, activity } = bodyInfo;
+  const ratio = CaloriesParameters[gender][activity];
+
+  try {
+    const _BMR = Object.values(BMR).map((func) => func(bodyInfo));
+
+    const min = Math.min(
+      ..._BMR.map((val) => (typeof val === 'string' ? Number(val) : Number(val[1])))
+    );
+
+    return new Decimal(min).times(ratio).round().toString();
+  } catch {
+    return '-';
+  }
 }
