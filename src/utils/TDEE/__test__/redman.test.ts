dde167bbedccb920ef0edbd852e9a57229adb08a
kevinhws <kevinshu1995@gmail.com>
2022-08-14 02:14:07 +0800

    test: 💍 utils/TDEE

---
+++ src/utils/TDEE/__test__/redman.test.ts
@@ -0,0 +1,39 @@
+import { calRedman } from '../redman';
+import testCases from './testCase';
+
+describe('test redman()', () => {
+  test.concurrent.each(testCases.testBasic)(
+    'Basic case - index: %#, %o',
+    async ({ expected: { redman }, ...bodyInfo }, expected) => {
+      expect(calRedman(bodyInfo)).toBe(redman);
+    }
+  );
+
+  test.concurrent.each(testCases.testDate)(
+    'Date cases - index: %#, %o',
+    async ({ expected: { redman }, ...bodyInfo }, expected) => {
+      expect(calRedman(bodyInfo)).toBe(redman);
+    }
+  );
+
+  test.concurrent.each(testCases.testGender)(
+    'Gender cases - index: %#, %o',
+    async ({ expected: { redman }, ...bodyInfo }, expected) => {
+      expect(calRedman(bodyInfo)).toBe(redman);
+    }
+  );
+
+  test.concurrent.each(testCases.testWeight)(
+    'Weight cases - index: %#, %o',
+    async ({ expected: { redman }, ...bodyInfo }, expected) => {
+      expect(calRedman(bodyInfo)).toBe(redman);
+    }
+  );
+
+  test.concurrent.each(testCases.complexCase)(
+    'Complex cases - index: %#, %o',
+    async ({ expected: { redman }, ...bodyInfo }, expected) => {
+      expect(calRedman(bodyInfo)).toBe(redman);
+    }
+  );
+});
