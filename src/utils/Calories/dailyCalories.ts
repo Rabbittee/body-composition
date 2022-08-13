0a814209d35de5721c15b0239512012da23b4c15
alpha <alphatero@icloud.com>
2022-08-13 22:07:14 +0800

    feat: 🎸 add calories caculator

--- src/utils/Calories/dailyCalories.ts
+++ src/utils/Calories/dailyCalories.ts
@@ -1,5 +1,28 @@
-import { BodyInfo } from 'models';
+import Decimal from 'decimal.js';
+import { BodyInfo, Activity, Gender } from 'models';
 
+import * as BMRFunc from '../BMR';
+import { CaloriesParameters } from './parameters';
+
+//round(min(0.007184*(height*0.725)*(weight*0.425)*24*(vlookup(BMI(age, gender))*(actionvalue(actionType, gender)))
 export function calDailyCalories(bodyInfo: BodyInfo) {
-  return '-';
+  const { gender, activity } = bodyInfo;
+
+  try {
+    const BMR = Object.values(BMRFunc).map((func) => func(bodyInfo));
+
+    const min = Math.min(
+      ...BMR.map((val) => (typeof val !== 'string' ? Number(val[1]) : Number(val)))
+    );
+
+    const index = Object.values(Activity).indexOf(activity);
+
+    if (gender === Gender.Male) {
+      return new Decimal(min).times(CaloriesParameters.Male[index]).round().toString();
+    }
+
+    return new Decimal(min).times(CaloriesParameters.Female[index]).round().toString();
+  } catch {
+    return '-';
+  }
 }
