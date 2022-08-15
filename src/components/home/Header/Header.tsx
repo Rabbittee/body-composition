cad7125c65a66fe953e26e2415ba9ba34f0516a8
Augustine <ashramwen@gmail.com>
2022-08-15 18:26:23 +0800

    feat: Update source link

--- src/components/home/Header/Header.tsx
+++ src/components/home/Header/Header.tsx
@@ -1,16 +1,12 @@
-export function Header() {
+import { CONFIG } from 'config';
 
+export function Header() {
   return (
     <div className="space-y-2">
       <div className="flex justify-between">
         <div className="space-x-2 text-blueGray-light">
           <span>Source:</span>
-          <a
-            href="https://www.facebook.com/100050186212776/posts/pfbid08kibVooUgdRwAn2LjWSRjzVJ5wzvjYncGqP2XvvR8vC9SYPqqH86HZ3uTZGqesy2l/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link"
-          >
+          <a href={CONFIG.sourceLink} target="_blank" rel="noopener noreferrer" className="link">
             王介立醫師
           </a>
         </div>
