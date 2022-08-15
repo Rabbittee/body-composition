a13d2feccd0743b728180242d7a285232a211983
Augustine <ashramwen@users.noreply.github.com>
2022-08-15 18:43:17 +0800

    feat: Update source link (#44)

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,10 @@
 export const CONFIG = {
   storageKey: 'rabbittee_bodyInfo',
+
+  // sourceLink:
+  //   'https://www.facebook.com/100050186212776/posts/pfbid08kibVooUgdRwAn2LjWSRjzVJ5wzvjYncGqP2XvvR8vC9SYPqqH86HZ3uTZGqesy2l/',
+  sourceLink: 'https://www.facebook.com/groups/1028435774570219/posts/1229546761125785/',
+
   gcp: {
     id: process.env.REACT_APP_GCP_ID as string,
     secret: process.env.REACT_APP_GCP_SECRET as string,
