9c14c25447e1ddd78b5566a2824e901cc7293a8d
Augustine <ashramwen@gmail.com>
2022-08-16 13:06:38 +0800

    refactor: Change locales to json files

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import TRANSLATIONS_ZH from 'locales/zhTW';
-import TRANSLATIONS_EN from 'locales/en';
+import TRANSLATIONS_ZH from 'locales/zhTW.json';
+import TRANSLATIONS_EN from 'locales/en.json';
 
 const resources = {
   en: {
