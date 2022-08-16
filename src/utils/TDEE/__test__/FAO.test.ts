d9e1cdcbe8f34a62be8762c2fe6a146d64e86f5e
Augustine <ashramwen@gmail.com>
2022-08-16 11:01:43 +0800

    refactor: Refactor tdee tests

--- src/utils/TDEE/__test__/FAO.test.ts
+++ src/utils/TDEE/__test__/FAO.test.ts
@@ -4,35 +4,35 @@ import testCases from './testCase';
 describe('test redman()', () => {
   test.concurrent.each(testCases.testBasic)(
     'Basic case - index: %#, %o',
-    async ({ expected: { FAO }, ...bodyInfo }, expected) => {
+    async ({ expected: { FAO }, ...bodyInfo }) => {
       expect(calFAO(bodyInfo)).toBe(FAO);
     }
   );
 
   test.concurrent.each(testCases.testDate)(
     'Date case - index: %#, %o',
-    async ({ expected: { FAO }, ...bodyInfo }, expected) => {
+    async ({ expected: { FAO }, ...bodyInfo }) => {
       expect(calFAO(bodyInfo)).toBe(FAO);
     }
   );
 
   test.concurrent.each(testCases.testGender)(
     'Gender case - index: %#, %o',
-    async ({ expected: { FAO }, ...bodyInfo }, expected) => {
+    async ({ expected: { FAO }, ...bodyInfo }) => {
       expect(calFAO(bodyInfo)).toBe(FAO);
     }
   );
 
   test.concurrent.each(testCases.testWeight)(
     'Weight case - index: %#, %o',
-    async ({ expected: { FAO }, ...bodyInfo }, expected) => {
+    async ({ expected: { FAO }, ...bodyInfo }) => {
       expect(calFAO(bodyInfo)).toBe(FAO);
     }
   );
 
   test.concurrent.each(testCases.complexCase)(
     'Complex case - index: %#, %o',
-    async ({ expected: { FAO }, ...bodyInfo }, expected) => {
+    async ({ expected: { FAO }, ...bodyInfo }) => {
       expect(calFAO(bodyInfo)).toBe(FAO);
     }
   );
